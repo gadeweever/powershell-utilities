cd8fd2466335c4754a81cee4a1a240c3bd3990fc
Gamal DeWeever <gadeweever@uchicago.edu>
2013-11-08 22:52:38 -0600

    before tables

--- pull-uc.ps1
+++ pull-uc.ps1
@@ -1,26 +1,24 @@
 #create object for web request
-$request_predicate = "/student-life/annual-events"
+$request_predicate = "/student-life/student-groups"
 $request_url = [string]::Concat("https://college.uchicago.edu", $request_predicate);
 $request = Invoke-WebRequest -Uri $request_url;
 $request.Links > "links.txt"
 $request.Content > "content.txt"
 $request.ParsedHTML > "parsedContent.txt"
 $request.Headers > "headers.txt"
+$request_final = [string]::Concat($request_predicate,"Final.txt");
 
-
+#create control variables
 $pattern = "";
 $search_index = 0;
-$links = ""
-
-$num_tag = 1;
-$got_div = $False;
+$links = "";
+$buffer = "";
 
 $search_txt = ".\content.txt"
 $search_obj = Select-String -Pattern "contentPage" -Path $search_txt
 
 $search_index = $search_obj.LineNumber[0];
-$search_range = $search_obj.LineNumber[1] - $search_obj.LineNumber[0];
-Write-Host "$search_index is your index";
+# Write-Host "$search_index is your index";
 
 $to_add = Select-String -Pattern "href=" -Path $search_txt;
 $num_links = $to_add.length;
@@ -31,35 +29,43 @@ for($i = 0; $i -le $num_links; $i++)
 		{
 			if($to_add[$i].LineNumber -le $search_obj[1].LineNumber)
 				{
-					$links = [string]::Concat($links, $to_add[$i].Line,"\n");
+					$links = [string]::Concat($links, $to_add[$i].Line,"`n");
 				}
 		}
 }
 
+# for purposes of the page, these tages are filtered out
+$links = $links.Replace("<h4>", "");
+$links = $links.Replace("</h4>", "");
+$links = $links.Replace("<h3>", "");
+$links = $links.Replace("</h3>", "");
+$links = $links.Replace("href=", "");
 
 
+# pass to create a new string with just links
+# $search_index = $links.length;
+# for($i = 0; $i -lt $search_index; $i++)
+# 	{
+# 		#check if we have a link tag, if so, begin writing
+# 		if(($links.substring($i,$i+2).compareTo("<a")))
+# 			{
+# 				$i = $i + 2;
+# 				#begin writing
+# 				while (-not ($links[$i].toString().compareTo(">")))
+# 					{
+# 						$buffer = $buffer + $links[$i].toString();
+# 						$i++;
+# 					}
+# 				$buffer = $buffer + "`n";
+					
+# 			}
+# 		else
+# 			{
+# 				$i++;
+# 			}
+
+# 	}
 
 
-# while($num_tag -ge 1)
-# {
-	
-# 	$to_add = Select-String -Pattern "href=" -Path $search_txt;
-# 	$links = [string]::Concat($links, $to_add.Line);
-# 	$got_div = Select-string -context $search_index -Pattern "<div>" -Path $search_txt -Quiet;
-# 	if($got_div)
-# 		{
-# 			$num_tag = $num_tag + 1;
-# 		}
-# 	$got_div = $False;
-# 	$got_div = Select-string -context $search_index -Pattern "</div>" -Path $search_txt -Quiet;
-# 	if($got_div)
-# 		{
-# 			$num_tag = $num_tag - 1;
-# 		}
-# 	$got_div = $False;
-# 	$search_index = $search_index + 1;
+$links > "student-groups.txt";
 
-
-# }
-
-$links > [string]::Concat($request_predicate,"Final.txt");
